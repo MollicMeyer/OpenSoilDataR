76e9173c06295eca23768f387d6c5cdc2a3a5aed
MollicMeyer <mpbohn@iastate.edu>
2025-07-24 13:02:45 -0500

    fixzonespeiid

--- R/zones_to_SPC.R
+++ R/zones_to_SPC.R
@@ -1,18 +1,18 @@
-#' Convert Zonal Summaries to SoilProfileCollection
+#' Convert Zonal Summary Raster Data to SoilProfileCollection
 #'
-#' @param rstack A `SpatRaster`
-#' @param zones An `sf` or `SpatVector` object with polygons
-#' @param stat Character. One or more of "mean", "min", "max", "median"
-#' @param id_column Column name in `zones` used as unique ID
-#' @return A `SoilProfileCollection` object
+#' @param rstack A `SpatRaster` stack from a fetch_*() function
+#' @param zones A polygon `sf` or `SpatVector` object defining zones
+#' @param stat Character. One or more statistics passed to `terra::zonal()` (e.g., "mean", "min")
+#' @param id_column Character. Name of the unique identifier column in zones (default: "Name")
+#' @return A SoilProfileCollection with one profile per zone
 #' @export
 zones_to_SPC <- function(rstack, zones, stat = "mean", id_column = "Name") {
   require(terra)
   require(dplyr)
   require(tidyr)
   require(aqp)
-  require(sf)
   require(stringr)
+  require(sf)
 
   depth_interval_lookup <- list(
     "0_5" = c("0_5", "0-5cm", "0_cm", "0-5"),
@@ -32,72 +32,56 @@ zones_to_SPC <- function(rstack, zones, stat = "mean", id_column = "Name") {
     "100_200" = c(100, 200)
   )
 
+  parse_layers <- function(df, idcol = "peiid") {
+    long_df <- df %>%
+      pivot_longer(
+        cols = -all_of(idcol),
+        names_to = "layer",
+        values_to = "value"
+      ) %>%
+      separate(
+        layer,
+        into = c("property", "top", "bottom"),
+        sep = "_",
+        remove = FALSE
+      ) %>%
+      mutate(
+        depth_label = paste0(top, "_", bottom),
+        matched_label = names(depth_interval_lookup)[
+          sapply(depth_interval_lookup, function(p) {
+            any(str_detect(
+              depth_label,
+              paste0("^(", paste(p, collapse = "|"), ")$")
+            ))
+          })
+        ],
+        hzdept = depth_range_lookup[[matched_label]][1],
+        hzdepb = depth_range_lookup[[matched_label]][2]
+      ) %>%
+      ungroup()
+
+    long_df %>%
+      select(all_of(idcol), hzdept, hzdepb, property, value) %>%
+      pivot_wider(names_from = property, values_from = value)
+  }
+
   if (!inherits(zones, "SpatVector")) {
     zones <- terra::vect(zones)
   }
-
-  if (!terra::same.crs(rstack, zones)) {
-    zones <- terra::project(zones, terra::crs(rstack))
+  if (!id_column %in% names(zones)) {
+    stop("id_column not found in zones.")
   }
-
-  zones <- zones[!is.na(zones[[id_column]]), ]
+  if (!same.crs(rstack, zones)) {
+    zones <- project(zones, crs(rstack))
+  }
 
   stat_list <- lapply(stat, function(s) terra::zonal(rstack, zones, fun = s))
   df <- Reduce(function(x, y) full_join(x, y, by = id_column), stat_list)
   df$peiid <- df[[id_column]]
 
-  long_df <- df %>%
-    pivot_longer(
-      cols = -c(peiid, all_of(id_column)),
-      names_to = "layer",
-      values_to = "value"
-    ) %>%
-    rowwise() %>%
-    mutate(
-      matched_label = {
-        matches <- sapply(depth_interval_lookup, function(pats) {
-          any(str_detect(
-            layer,
-            paste0("_(", paste(pats, collapse = "|"), ")$")
-          ))
-        })
-        if (any(matches)) {
-          names(depth_interval_lookup)[which(matches)[1]]
-        } else {
-          NA_character_
-        }
-      },
-      hzdept = if (!is.na(matched_label)) {
-        depth_range_lookup[[matched_label]][1]
-      } else {
-        NA_real_
-      },
-      hzdepb = if (!is.na(matched_label)) {
-        depth_range_lookup[[matched_label]][2]
-      } else {
-        NA_real_
-      },
-      variable = if (!is.na(matched_label)) {
-        str_remove(
-          layer,
-          paste0(
-            "_(",
-            paste(depth_interval_lookup[[matched_label]], collapse = "|"),
-            ")$"
-          )
-        )
-      } else {
-        NA_character_
-      }
-    ) %>%
-    ungroup()
+  hz <- parse_layers(df, idcol = "peiid")
+  depths(hz) <- peiid ~ hzdept + hzdepb
+  site(hz) <- df %>% select(peiid)
 
-  hz_data <- long_df %>%
-    select(peiid, hzdept, hzdepb, variable, value) %>%
-    pivot_wider(names_from = variable, values_from = value)
-
-  depths(hz_data) <- peiid ~ hzdept + hzdepb
-  site(hz_data) <- df %>% select(peiid)
-
-  return(hz_data)
+  return(hz)
 }
