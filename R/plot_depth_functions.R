cde81cd31254f459e18b00b6a2280febcc3f2bc1
MollicMeyer <mpbohn@iastate.edu>
2025-07-24 16:15:36 -0500

    insert lookup table

--- R/plot_depth_functions.R
+++ R/plot_depth_functions.R
@@ -2,7 +2,7 @@
 #'
 #' @param spc_list A list of SoilProfileCollection objects to compare.
 #' @param source_labels A character vector of labels (e.g., "PSP", "SGO") matching `spc_list`.
-#' @param variables Character vector of horizon-level variables to plot.
+#' @param variables Character vector of standardized property names to plot (e.g., "sand", "clay").
 #' @param slab_structure Numeric vector of depth breaks (e.g., c(0, 5, 15, 30, 60, 100)).
 #'
 #' @return A lattice depth function plot (mean ± SD) comparing groups.
@@ -21,33 +21,308 @@ plot_depth_functions <- function(
   library(lattice)
   library(latticeExtra)
 
-  # Assign source label to each SPC
+  # Property Lookup Table for use in s.zonalstats()
+  property_lookup <- list(
+    # Water Retention
+    "wtenthbar" = c(
+      PSP = NA,
+      SG2 = NA,
+      SOL = NA,
+      CSRL = NA,
+      SGO = "wtenthbar_r"
+    ),
+    "wthirdbar" = c(
+      PSP = NA,
+      SG2 = NA,
+      SOL = NA,
+      CSRL = NA,
+      SGO = "wthirdbar_r"
+    ),
+    "wfifteenbar" = c(
+      PSP = NA,
+      SG2 = NA,
+      SOL = NA,
+      CSRL = NA,
+      SGO = "wfifteenbar_r"
+    ),
+    "awc" = c(PSP = NA, SG2 = NA, SOL = "awc_r", CSRL = NA, SGO = "awc_r"),
+
+    # Phosphorus
+    "pbray1" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "pbray1_r"),
+    "ptotal" = c(
+      PSP = NA,
+      SG2 = NA,
+      SOL = "ptotal_r",
+      CSRL = NA,
+      SGO = "ptotal_r"
+    ),
+    "ph2osoluble" = c(
+      PSP = NA,
+      SG2 = NA,
+      SOL = NA,
+      CSRL = NA,
+      SGO = "ph2osoluble_r"
+    ),
+    "poxalate" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "poxalate_r"),
+
+    # Bulk Density
+    "bd" = c(
+      PSP = "bd_mean",
+      SG2 = "bdod",
+      SOL = "dbovendry_r",
+      CSRL = "bulk_density",
+      SGO = "dbovendry_r"
+    ),
+    "bd_tenthbar" = c(
+      PSP = NA,
+      SG2 = NA,
+      SOL = NA,
+      CSRL = NA,
+      SGO = "dbtenthbar_r"
+    ),
+    "bd_thirdbar" = c(
+      PSP = NA,
+      SG2 = NA,
+      SOL = NA,
+      CSRL = NA,
+      SGO = "dbthirdbar_r"
+    ),
+    "bd_fifteenbar" = c(
+      PSP = NA,
+      SG2 = NA,
+      SOL = NA,
+      CSRL = NA,
+      SGO = "dbfifteenbar_r"
+    ),
+
+    # Texture
+    "clay" = c(
+      PSP = "clay_mean",
+      SG2 = "clay",
+      SOL = "claytotal",
+      CSRL = "clay_profile",
+      SGO = "claytotal_r"
+    ),
+    "sand" = c(
+      PSP = "sand_mean",
+      SG2 = "sand",
+      SOL = "sandtotal",
+      CSRL = "sand_profile",
+      SGO = "sandtotal_r"
+    ),
+    "silt" = c(
+      PSP = "silt_mean",
+      SG2 = "silt",
+      SOL = "silttotal",
+      CSRL = "silt_profile",
+      SGO = "silttotal_r"
+    ),
+    "sandco" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sandco_r"),
+    "sandfine" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sandfine_r"),
+    "sandmed" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sandmed_r"),
+    "sandvc" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sandvc_r"),
+    "sandvf" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sandvf_r"),
+    "siltco" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "siltco_r"),
+    "siltfine" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "siltfine_r"),
+
+    # Organic Carbon
+    "som" = c(
+      PSP = "om_mean",
+      SG2 = NA,
+      SOL = NA,
+      CSRL = "som_max",
+      SGO = "om_r"
+    ),
+    "soc" = c(
+      PSP = "soc",
+      SG2 = "soc",
+      SOL = "soc",
+      CSRL = "soc_max",
+      SGO = "oc_r"
+    ),
+    "soc_stock" = c(
+      PSP = NA,
+      SG2 = "ocs",
+      SOL = NA,
+      CSRL = "soc_stock",
+      SGO = NA
+    ),
+
+    # Chemistry
+    "ph" = c(
+      PSP = "ph_mean",
+      SG2 = "phh2o",
+      SOL = "ph1to1h2o",
+      CSRL = "ph_profile",
+      SGO = "ph1to1h2o_r"
+    ),
+    "ph_cacl2" = c(
+      PSP = NA,
+      SG2 = NA,
+      SOL = NA,
+      CSRL = NA,
+      SGO = "ph01mcacl2_r"
+    ),
+    "ph_oxidized" = c(
+      PSP = NA,
+      SG2 = NA,
+      SOL = NA,
+      CSRL = NA,
+      SGO = "phoxidized_r"
+    ),
+    "cec" = c(
+      PSP = NA,
+      SG2 = "cec",
+      SOL = "cec7",
+      CSRL = "cec_profile",
+      SGO = "cec7_r"
+    ),
+    "ecec" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "ecec_r"),
+    "ec" = c(PSP = NA, SG2 = NA, SOL = "ec", CSRL = "ec_profile", SGO = "ec_r"),
+    "ec15" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "ec15_r"),
+    "esp" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "esp_r"),
+    "sar" = c(PSP = NA, SG2 = NA, SOL = "sar", CSRL = "sar", SGO = "sar_r"),
+    "gypsum" = c(
+      PSP = NA,
+      SG2 = NA,
+      SOL = "gypsum",
+      CSRL = NA,
+      SGO = "gypsum_r"
+    ),
+    "caco3" = c(
+      PSP = NA,
+      SG2 = NA,
+      SOL = "caco3",
+      CSRL = "caco3",
+      SGO = "caco3_r"
+    ),
+
+    # Extractables
+    "extracid" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "extracid_r"),
+    "extral" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "extral_r"),
+    "aloxalate" = c(
+      PSP = NA,
+      SG2 = NA,
+      SOL = NA,
+      CSRL = NA,
+      SGO = "aloxalate_r"
+    ),
+    "feoxalate" = c(
+      PSP = NA,
+      SG2 = NA,
+      SOL = NA,
+      CSRL = NA,
+      SGO = "feoxalate_r"
+    ),
+
+    # Mechanics
+    "lep" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "lep_r"),
+    "ll" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "ll_r"),
+    "pi" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "pi_r"),
+
+    # Rock Fragments
+    "frag3to10" = c(
+      PSP = NA,
+      SG2 = NA,
+      SOL = NA,
+      CSRL = NA,
+      SGO = "frag3to10_r"
+    ),
+    "fraggt10" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "fraggt10_r"),
+
+    # Fiber
+    "fiberrubbedpct" = c(
+      PSP = NA,
+      SG2 = NA,
+      SOL = NA,
+      CSRL = NA,
+      SGO = "fiberrubbedpct_r"
+    ),
+    "fiberunrubbedpct" = c(
+      PSP = NA,
+      SG2 = NA,
+      SOL = NA,
+      CSRL = NA,
+      SGO = "fiberunrubbedpct_r"
+    ),
+
+    # Sieve
+    "sieveno10" = c(
+      PSP = NA,
+      SG2 = NA,
+      SOL = NA,
+      CSRL = NA,
+      SGO = "sieveno10_r"
+    ),
+    "sieveno200" = c(
+      PSP = NA,
+      SG2 = NA,
+      SOL = NA,
+      CSRL = NA,
+      SGO = "sieveno200_r"
+    ),
+    "sieveno4" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "sieveno4_r"),
+    "sieveno40" = c(
+      PSP = NA,
+      SG2 = NA,
+      SOL = NA,
+      CSRL = NA,
+      SGO = "sieveno40_r"
+    ),
+
+    # Hydrology
+    "ksat" = c(
+      PSP = "ksat_mean",
+      SG2 = NA,
+      SOL = NA,
+      CSRL = "ksat_max",
+      SGO = "ksat_r"
+    ),
+    "wsatiated" = c(
+      PSP = NA,
+      SG2 = NA,
+      SOL = NA,
+      CSRL = NA,
+      SGO = "wsatiated_r"
+    ),
+
+    # Erosion
+    "kw" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "kwfact"),
+    "kffact" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "kffact"),
+    "kifact" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "kifact"),
+    "krfact" = c(PSP = NA, SG2 = NA, SOL = NA, CSRL = NA, SGO = "krfact")
+  )
+
+  # Rename variable names in each SPC based on lookup
   for (i in seq_along(spc_list)) {
-    source_name <- source_labels[i]
-    profile_id(spc_list[[i]]) <- paste0(
-      source_name,
-      "_",
-      profile_id(spc_list[[i]])
-    )
-    site(spc_list[[i]])$source <- source_name
+    src <- source_labels[i]
+    spc <- spc_list[[i]]
+
+    for (v in variables) {
+      actual_name <- property_lookup[[v]][[src]]
+      if (!is.na(actual_name) && actual_name %in% horizonNames(spc)) {
+        names(horizons(spc))[names(horizons(spc)) == actual_name] <- v
+      }
+    }
+
+    profile_id(spc) <- paste0(src, "_", profile_id(spc))
+    site(spc)$source <- src
+    spc_list[[i]] <- spc
   }
 
-  # Combine SPCs using aqp::combine
+  # Combine SPCs
   combined_spc <- do.call(aqp::combine, spc_list)
 
-  # Remove any residual `source` from horizons
   if ("source" %in% names(horizons(combined_spc))) {
     horizons(combined_spc)$source <- NULL
   }
 
-  # Define slab summary function
   mean.and.sd <- function(values) {
     m <- mean(values, na.rm = TRUE)
     s <- sd(values, na.rm = TRUE)
     c(mean = m, lower = m - s, upper = m + s)
   }
 
-  # Compute slabbed summary
   slab_list <- lapply(variables, function(var) {
     slab(
       combined_spc,
@@ -58,7 +333,6 @@ plot_depth_functions <- function(
   })
   slab_df <- bind_rows(slab_list)
 
-  # Okabe-Ito colorblind-friendly palette (max 8 groups)
   okabe_ito <- c(
     "#E69F00",
     "#56B4E9",
@@ -69,14 +343,8 @@ plot_depth_functions <- function(
     "#CC79A7",
     "#999999"
   )
+  palette_colors <- okabe_ito[seq_len(length(unique(slab_df$source)))]
 
-  group_levels <- unique(slab_df$source)
-  n_groups <- length(group_levels)
-
-  # Set color palette
-  palette_colors <- okabe_ito[seq_len(n_groups)]
-
-  # Build lattice plot
   xyplot(
     bottom ~ mean | variable,
     data = slab_df,
@@ -94,10 +362,7 @@ plot_depth_functions <- function(
       y = list(alternating = 3)
     ),
     par.settings = list(
-      superpose.line = list(
-        col = palette_colors,
-        lwd = 2
-      )
+      superpose.line = list(col = palette_colors, lwd = 2)
     ),
     panel = panel.depth_function,
     prepanel = prepanel.depth_function,
